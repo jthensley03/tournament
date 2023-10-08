a998e7fc3ece446c7b616371efacb319c9a91b39
jthensley03 <jasonh@udel.edu>
2023-10-08 15:58:06 -0400

    added firebase hosting

--- js/index.js
+++
@@ -1,3 +0,0 @@
-/* 
-    this is a js file
-*/
---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn9lRd9JO3kLc2vXAljeRmncae337qCUY",
+  authDomain: "cpeg470-tournament.firebaseapp.com",
+  projectId: "cpeg470-tournament",
+  storageBucket: "cpeg470-tournament.appspot.com",
+  messagingSenderId: "795525484702",
+  appId: "1:795525484702:web:4773cfa317f9993a22ab8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
